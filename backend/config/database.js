7e834ced95ee2b7c9817e7112e0be403383fccec
Ben Kang <ben-mini@bens-mac-mini.lan>
2021-04-11 19:46:53 -0700

    Convert strapi backend to postgresql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,23 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'bookshelf',
+            settings: {
+                client: 'postgres',
+                host: env('DATABASE_HOST', 'localhost'),
+                port: env.int('DATABASE_PORT', 5432),
+                database: env('DATABASE_NAME', 'goong'),
+                username: env('DATABASE_USERNAME', 'goong'),
+                password: env('DATABASE_PASSWORD', 'skgoong'),
+                schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+                ssl: {
+                    rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+                },
+            },
+            options: {
+                ssl: env.bool('DATABASE_SSL', false),
+            },
+        },
     },
-  },
-});
+})
