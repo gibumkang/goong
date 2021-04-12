43ebfcc7b9a860a9b476551a381ea3ba75f18b68
Ben Kang <ben-mini@bens-mac-mini.lan>
2021-04-11 19:56:53 -0700

    Finalize setup for heroku

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,19 +4,11 @@ module.exports = ({ env }) => ({
         default: {
             connector: 'bookshelf',
             settings: {
-                client: 'postgres',
-                host: env('DATABASE_HOST', 'localhost'),
-                port: env.int('DATABASE_PORT', 5432),
-                database: env('DATABASE_NAME', 'goong'),
-                username: env('DATABASE_USERNAME', 'goong'),
-                password: env('DATABASE_PASSWORD', 'skgoong'),
-                schema: env('DATABASE_SCHEMA', 'public'), // Not Required
-                ssl: {
-                    rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-                },
+                client: 'sqlite',
+                filename: env('DATABASE_FILENAME', '.tmp/data.db'),
             },
             options: {
-                ssl: env.bool('DATABASE_SSL', false),
+                useNullAsDefault: true,
             },
         },
     },
