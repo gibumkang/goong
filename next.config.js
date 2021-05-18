972bb9b6a91d4ff7c7b6b0b3f8c3755906c67f6d
Ben Kang <ben-mini@bens-mac-mini.lan>
2021-05-17 20:22:51 -0700

    Remove connection to strapi and apollojs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 const path = require('path')
 module.exports = {
-    env: {
-        BASE_URL: 'https://studiokanvas-goong.herokuapp.com',
-        API_URL: 'https://studiokanvas-goong.herokuapp.com/graphql',
-        NEXT_PUBLIC_API_URL: 'http://localhost:1337',
-    },
     images: {
-        domains: ['localhost', 'https://via.placeholder.com', 'goonglasvegas.com'],
+        domains: ['localhost', 'https://via.placeholder.com'],
     },
     webpack: (config) => {
         config.resolve.alias['components'] = path.join(__dirname, 'components')
         config.resolve.alias['public'] = path.join(__dirname, 'public')
         return config
     },
-    target: 'serverless'
 }
